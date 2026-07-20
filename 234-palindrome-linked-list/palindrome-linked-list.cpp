8af3bce8928ebfed356ca6709057e1c64c8caa42
Litesh-Goyal <goyallitesh195@gmail.com>
2026-07-20 11:36:22 +0530

    Time: 4 ms (49.44%) | Memory: 132.1 MB (7.99%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) 
+    {
+        if(head==nullptr){return false;}
+        if(head->next==nullptr){return true;}
+        vector<int> v;
+        ListNode* temp=head;
+        while(temp!=nullptr)
+        {
+            v.push_back(temp->val);
+            temp=temp->next;
+        }
+        for(int i=0;i<v.size()/2;i++)
+        {
+            if(v[i]!=v[v.size() -i -1]){return false;}
+        }
+        return true;
+        // if(head==nullptr || ( head->next!=nullptr && head->next->next==nullptr && head->val!=head->next->val)){return false;}
+        // if(head->next==nullptr){return true;}
+        // ListNode* temp=head;
+        // vector<int> v;
+        // while(temp!=nullptr)
+        // {
+        //     v.push_back(temp->val);
+        //     temp=temp->next;
+        // }
+        // int n=v.size();
+        // for(int i=0;i<n/2;i++)
+        // {
+        //     if(v[i]!=v[n-i-1]){return false;}
+        // }
+        // return true;
+        
+    }
+};
