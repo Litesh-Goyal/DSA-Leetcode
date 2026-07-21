1de23dd094b0f29a4f5b5edef1b04f260c88ccc9
Litesh-Goyal <goyallitesh195@gmail.com>
2026-07-21 12:14:34 +0530

    Time: 48 ms (5.67%) | Memory: 137.3 MB (5.12%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -14,33 +14,50 @@ public:
     {
         if(head==nullptr){return false;}
         if(head->next==nullptr){return true;}
-        vector<int> v;
-        ListNode* temp=head;
-        while(temp!=nullptr)
-        {
-            v.push_back(temp->val);
-            temp=temp->next;
-        }
-        for(int i=0;i<v.size()/2;i++)
-        {
-            if(v[i]!=v[v.size() -i -1]){return false;}
-        }
-        return true;
-        // if(head==nullptr || ( head->next!=nullptr && head->next->next==nullptr && head->val!=head->next->val)){return false;}
-        // if(head->next==nullptr){return true;}
-        // ListNode* temp=head;
         // vector<int> v;
+        // ListNode* temp=head;
         // while(temp!=nullptr)
         // {
         //     v.push_back(temp->val);
         //     temp=temp->next;
         // }
-        // int n=v.size();
-        // for(int i=0;i<n/2;i++)
+        // for(int i=0;i<v.size()/2;i++)
         // {
-        //     if(v[i]!=v[n-i-1]){return false;}
+        //     if(v[i]!=v[v.size() -i -1]){return false;}
         // }
         // return true;
+        ListNode* temp1=head;
+        ListNode* temp2=head;
+        while(temp2->next!=nullptr && temp2->next->next!=nullptr)
+        {
+            temp1=temp1->next;
+            temp2=temp2->next->next;
+        }
+        int c=0;
         
+        temp1=temp1->next;
+        temp2=temp1;
+        while(temp2!=nullptr)
+        {
+            if(c==0)
+            {
+                temp1=new ListNode(temp2->val);
+                c++;
+            }
+            else
+            {
+                temp1=new ListNode(temp2->val,temp1);
+            }
+            temp2=temp2->next;
+        }
+        temp2=head;
+        while(temp1!=nullptr)
+        {
+            if(temp2->val != temp1->val){return false;}
+            temp2=temp2->next;
+            temp1=temp1->next;
+        }
+        return true;
+
     }
 };
