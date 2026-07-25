0eb2b5bb6b0057130575865705e292d138e767e7
Litesh-Goyal <goyallitesh195@gmail.com>
2026-07-25 14:09:46 +0530

    Time: 5 ms (29.07%) | Memory: 12 MB (11.95%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine)
+    {
+        if(ransomNote.length()>magazine.length()){return false;}
+        unordered_map<char,int> mp;
+        for(int i=0;i<magazine.length();i++)
+        {
+            if(mp.count(magazine[i])==0)
+            {
+                mp[magazine[i]]=1;
+            }
+            else{mp[magazine[i]]+=1;}
+        }
+        for(int i=0;i<ransomNote.length();i++)
+        {
+            if(mp.count(ransomNote[i])==0 || mp[ransomNote[i]]==0)
+            {
+                return false;
+            }
+            else{mp[ransomNote[i]]-=1;}
+        }
+        return true;
+
+        
+    }
+};
