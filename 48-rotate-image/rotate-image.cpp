2cd2bc6afc52ab0c329907b524213af63c5ce787
Litesh-Goyal <goyallitesh195@gmail.com>
2026-09-01 11:49:21 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (9.26%) - LeetSync

---
+++ 48-rotate-image/rotate-image.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& m) 
+    {
+        int n=m.size();
+        for(int i=0;i<n/2;i++)
+        {
+            swap(m[i],m[n-i-1]);
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                swap(m[i][j],m[j][i]);
+            }
+        }
+        return ;
+        
+    }
+};
