47e7a507754a9ef435b63248867bab78eba23fc9
Litesh-Goyal <goyallitesh195@gmail.com>
2026-09-08 12:00:44 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (81.23%) - LeetSync

---
+++ 792-binary-search/binary-search.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int t) 
+    {
+        int left=0;
+        int right=nums.size()-1;
+        while(left<=right)
+        {
+            int mid=left+ (right-left)/2;
+            if(nums[mid]==t){return mid;}
+            else if(nums[mid]<t){left=mid+1;}
+            else if (nums[mid]>t){right=mid-1;}         
+        }        
+        return -1;
+    }
+};
